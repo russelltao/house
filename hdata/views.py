0491a010b096a9c6b35bd0726a934a1a3f161eb0
Administrator <admin@example.com>
2017-10-10 11:24:55 +0800

    use pyecharts instead of origin echart

--- hdata/views.py
+++ hdata/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import LjSecondChengjiao
 from django.db.models import Count, Min, Sum, Avg,StdDev,Max
+from pyecharts import Line
+
 
 # Create your views here.
 class OverviewStatView(TemplateView):
@@ -50,18 +52,62 @@ class OverviewStatView(TemplateView):
                 unitprice_series[code].append(unit_price__avg)
                 totalprice_sum_series[code].append(totalprice)
 
-        context['ljcj_cnt_by_dealdate'] = {
-                'series': sellcnt_series,
-                'xAxis': [dc['deal_date'].strftime('%Y%m%d') for dc in alldays]
-        }
+        day_attrs = [dc['deal_date'].strftime('%Y%m%d') for dc in alldays]
 
-        context['ljcj_unitprice_by_dealdate'] = {
-                'series': unitprice_series,
-                'xAxis': [dc['deal_date'].strftime('%Y%m%d') for dc in alldays]
-        }
-        context['ljcj_totalprice_by_dealdate'] = {
-                'series': totalprice_sum_series,
-                'xAxis': [dc['deal_date'].strftime('%Y%m%d') for dc in alldays]
-        }
+        unitprice_byarea_line = Line(title="二手房区域日成交均价趋势",subtitle='杭州地区各行政区域统计',
+                    width=1800)
+        for k,v in unitprice_series.items():
+            unitprice_byarea_line.add("%s"%(k), day_attrs, v, mark_point=["average"])
+
+        dealcnt_byarea_line = Line(title="二手房区域日成交次数趋势",subtitle='杭州地区各行政区域统计',
+                    width=1800)
+        for k,v in sellcnt_series.items():
+            dealcnt_byarea_line.add("%s"%(k), day_attrs, v, mark_point=["average"])
+
+        totalprice_byarea_line = Line(title="二手房区域日成交次数趋势",subtitle='杭州地区各行政区域统计',
+                    width=1800)
+        for k,v in totalprice_sum_series.items():
+            totalprice_byarea_line.add("%s"%(k), day_attrs, v, mark_point=["average"])
+
+        attr = []
+        unitPriceAvg = []
+        unitPriceMax = []
+        dealCnt = []
+        totalpriceSum = []
+        ljcj_stats = LjSecondChengjiao.objects.values('deal_date') \
+            .annotate(Count('id'),Avg('unit_price'),Max('unit_price'),Sum('total_price'),).order_by('deal_date')
+        for ls in ljcj_stats:
+            attr.append(ls['deal_date'])
+            unitPriceAvg.append(ls['unit_price__avg'])
+            unitPriceMax.append(ls['unit_price__max'])
+            dealCnt.append(ls['id__count'])
+            totalpriceSum.append(ls['total_price__sum'])
+
+        unitprice_line = Line(title="二手房日成交均价趋势",subtitle='杭州地区链家数据统计',
+                    width=1800)
+        unitprice_line.add("单价均价", attr, unitPriceAvg, mark_point=["average"])
+        unitprice_line.add("单价最高价", attr, unitPriceMax, mark_point=["average"])
+
+        dealcnt_line = Line(title="二手房日成交次数趋势",subtitle='杭州地区链家数据统计',
+                    width=1800)
+        dealcnt_line.add("成交次数", attr, dealCnt, )
+
+        totalprice_line = Line(title="二手房日成交总价趋势",subtitle='杭州地区链家数据统计',
+                    width=1800)
+        totalprice_line.add("总价", attr, dealCnt, )
+
+        context['unitprice_line'] = unitprice_line.render_embed()
+        context['dealcnt_line'] = dealcnt_line.render_embed()
+        context['unitprice_byarea_line'] = unitprice_byarea_line.render_embed()
+        context['dealcnt_byarea_line'] = dealcnt_byarea_line.render_embed()
+        context['totalprice_byarea_line'] = totalprice_byarea_line.render_embed()
+        context['totalprice_line'] = totalprice_line.render_embed()
+
+        context['host'] = 'http://localhost:8000'
+        context['script_list'] = unitprice_line.get_js_dependencies()+ \
+                                 unitprice_byarea_line.get_js_dependencies()+ \
+                                 dealcnt_byarea_line.get_js_dependencies()+\
+                                 dealcnt_line.get_js_dependencies()+\
+                                 totalprice_line.get_js_dependencies()
 
         return context
